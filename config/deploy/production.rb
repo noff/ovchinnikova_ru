aeecd11828bc4dc96f76ef91e884141c4052e3a4
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2020-04-24 20:24:20 +0300

    initial version

---
+++ config/deploy/production.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+set :user, "ovchinnikova"
+server "ovchinnikova.ru", user: fetch(:user), roles: %w[app db web]
+
+set :ssh_options,
+    forward_agent: true,
+    port: 21212
+
+set :application, "ovchinnikova"
+set :deploy_to, "/home/ovchinnikova/#{fetch(:application)}"
+set :branch, "master"
+set :rails_env, "production"
+
+set :default_environment,
+    "RUBY_VERSION" => "ruby 2.4.4"
