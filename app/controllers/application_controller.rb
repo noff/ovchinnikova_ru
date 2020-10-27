ebf9dfea285b80a22aa6182212f19c08f1a1a3f5
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2020-10-27 14:28:22 +0300

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  http_basic_authenticate_with name: Rails.application.credentials.admin_user, password: Rails.application.credentials.admin_pass, if: :admin_controller?
+
+  def admin_controller?
+    self.class < ActiveAdmin::BaseController
+  end
+
 end
